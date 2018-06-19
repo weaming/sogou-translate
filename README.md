61ad4d3e7f444ff01f065baec72788a449520889
Jiadeng Ruan <jiadeng@16financial.com>
2018-06-19 20:09:10 +0800

    add sogou command

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Sogou::Translate
-
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/sogou/translate`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+Sogou translation API and CLI tool.
 
 ## Installation
 
@@ -22,7 +19,11 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+```
+sogou translate TEXT
+```
+
+API `Sogou::Translate.translate(text)`
 
 ## Development
 
---
+++ lib/sogou/translate/cli.rb
@@ -0,0 +1,29 @@
+require 'thor'
+require "sogou/translate/get"
+
+def red(text)
+    code = 31
+    return "\e[#{code}m#{text}\033[0m"
+end
+
+
+def green(text)
+    code = 32
+    return "\e[#{code}m#{text}\033[0m"
+end
+
+
+module Sogou
+    module Translate
+        class CLI < Thor
+            desc "trans TEXT", "translate TEXT"
+            def translate(text='文章本天成，妙手偶得之')
+                js = Sogou::Translate.translate(text)
+                puts "  #{green("From")}: #{js['translate']['from']}"
+                puts "    #{green("To")}: #{js['translate']['to']}"
+                puts "  #{green("Text")}: #{js['translate']['text']}"
+                puts "#{green("Result")}: #{js['translate']['dit']}"
+            end
+        end
+    end
+end
